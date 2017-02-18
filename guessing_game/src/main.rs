0cbf23a3d525d5d39f59c4177c2bda5991413185
Hongbin Zhou <hongbinzhou@hotmail.com>
2017-02-18 19:29:11 +0800

    use rand and loop guess.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,30 @@
+extern crate rand;
+
 use std::io;
 use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
 
     println!("Game start!");
-    let magic_number = 40;
+    let magic_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut input_raw = String::new();
-    io::stdin().read_line(&mut input_raw).expect("Failed to read input");
-    let input = input_raw.trim();
-    match input.cmp(&magic_number.to_string()) {
-        Ordering::Less => println!("{} is too small!", input),
-        Ordering::Equal    => println!("{} is too small!", input),
-        Ordering::Greater => println!("{} is good!", input),
+    loop {
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read input");
+        let guess : u32 = guess.trim().parse().expect("Please input an integer!");
+
+        match guess.cmp(&magic_number) {
+            Ordering::Less  => println!("{} is too small!", guess),
+            Ordering::Equal => {
+                println!("{} is good!", guess);
+                break;
+            },
+            Ordering::Greater => println!("{} is too big!", guess),
+        }
     }
-    println!("Game over!");
+
+    println!("Game over, The magic number is: {}", magic_number);
 
 }
