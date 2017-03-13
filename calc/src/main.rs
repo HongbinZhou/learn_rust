1d406cc4ee98153ee6294600f6eeacddb8f4e541
Hongbin Zhou <hongbinzhou@hotmail.com>
2017-03-13 23:28:49 +0800

    able to calc int +,-,*,/.
    learn:
    - debug use lldb or gdb. (only rust-lldb work on mac)
    - use: "cargo test -- --nocapture" to output all debug messages.

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -11,22 +11,40 @@ fn main() {
 
 fn calc(input:&str) -> i32 {
     // let mut raw = String::from(input);
+    println!("\n---- {}", &input);
+    let mut result: i32 = 0;
+    let mut last_op: char = ' ';
     for char in input.chars() {
         match char {
-            ' ' => println!("blank"),
-            '+' => println!("plus"),
-            '0'...'9'   => println!("{}", char),
+            ' ' => {}
+            '+' | '-' | '*' | '/' => {
+                last_op = char;
+            }
+            '0'...'9' => {
+                let num = char.to_digit(10).unwrap() as i32;
+                match last_op {
+                    '+' => result += num,
+                    '-' => result -= num,
+                    '*' => result *= num,
+                    '/' => result /= num,
+                    _   => result = num,
+                }
+                println!("current num: {}, last op: {}, result is:{}",
+                         num, last_op, result);
+            },
             _ => println!("unknown"),
         }
 
     }
-    // println!{"Input str: {}", chars.as_str()};
-    1
+    result
 }
 
 #[test]
 fn test_calc() {
-    let result = calc(" 1 + 2  ");
-    assert_eq!(result, 3);
+    assert_eq!(calc(" 1 + 2  "), 3);
+    assert_eq!(calc(" 0 - 3  "), -3);
+    assert_eq!(calc(" 2 / 2  "), 1);
+    assert_eq!(calc(" 2 * 2  "), 4);
+    assert_eq!(calc(" 2 * 3 * 4 /2 "), 12);
 }
 
